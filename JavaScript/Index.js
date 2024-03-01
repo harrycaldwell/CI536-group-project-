1ca1feb181c4047ca4f92ca6dab3070372146838
Harry Caldwell <harrycaldwell2011@hotmail.co.uk>
2024-03-01 11:42:54 +0000

    CSS and Javascript
    added some bits that are not functional yet to my knowledge

---
+++ JavaScript/Index.js
@@ -0,0 +1,13 @@
+//This function is for the sticky Navbar
+window.onscroll = function() {stickyNav()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickyNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
