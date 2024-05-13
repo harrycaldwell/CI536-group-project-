a97ad075c6576dc52d1e1bd27c4f9f9b1a0e16c2
Harry Caldwell <harrycaldwell2011@hotmail.co.uk>
2024-05-13 14:43:22 +0100

    fixed logout
    fixed logout

--- Logout.php
+++ Logout.php
@@ -1,6 +1,17 @@
 <?php
+// Start the session to access session variables
 session_start();
-unset($_COOKIE["username"]);
-header(Location:"https://hc899.brighton.domains/Groupproject/Index.php");
-exit();
+
+// Unset all session variables
+$_SESSION = array();
+
+// Destroy the session
+session_destroy();
+
+// Unset the username cookie by setting its expiration time to the past
+setcookie("username", "", time() - 3600, "/");
+
+// Redirect the user to the login page or any other desired page
+header("Location: Index.php");
+exit;
 ?>
